cce68ed9c14667a5f94a6e0eb8f55cbc07943bd7
elCaccia <micheletto.federico@gmail.com>
2021-05-04 09:46:40 +0200

    feat: backend working

--- README.md
+++ README.md
@@ -2,6 +2,102 @@
 
 This is a simple fullstack program that
 generate random number like the one used
-in the Enalotto.
+in the Enalotto
+
+## Install
+
+```bash
+npm install
+```
+
+## Usaage backend
+
+```bash
+ts-node main.ts
+```
+
+exaple output:
+
+```
+{
+  "Bari": [
+    36,
+    84,
+    72,
+    68,
+    77
+  ],
+  "Cagliari": [
+    62,
+    89,
+    13,
+    45,
+    12
+  ],
+  "Firenze": [
+    97,
+    13,
+    63,
+    93,
+    48
+  ],
+  "Genova": [
+    85,
+    73,
+    89,
+    70,
+    81
+  ],
+  "Milano": [
+    32,
+    91,
+    62,
+    66,
+    51
+  ],
+  "Napoli": [
+    12,
+    9,
+    54,
+    51,
+    80
+  ],
+  "Palermo": [
+    17,
+    87,
+    35,
+    43,
+    11
+  ],
+  "Roma": [
+    46,
+    38,
+    75,
+    7,
+    99
+  ],
+  "Torino": [
+    89,
+    6,
+    41,
+    38,
+    76
+  ],
+  "Venezia": [
+    47,
+    32,
+    98,
+    59,
+    57
+  ],
+  "Nazionale": [
+    82,
+    21,
+    16,
+    22,
+    68
+  ]
+}
+```
 
 devoleped by Federico Micheletto
--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
-console.log("hello world");
+import { resourceLimits } from "node:worker_threads";
+
+function RNG(min:number, max:number){
+    const rng = Math.random();
+    return Math.trunc(rng * (max - min) + min);
+}
+
+// function RNGDec(min:number, max:number, precision:number){
+//     if(precision < 0){
+//         throw new Error(`The precision number (${precision}) is negative`);
+//     }
+//     if(!Number.isInteger(precision)){
+//         throw new Error(`The precision number (${precision}) is not integer`);
+//     }
+//     return RNG(min, max).toFixed(precision);
+// }
+
+function RNGSequence (len: number, min:number, max:number){
+    if (len > max - min){
+        throw new Error(`cannot find ${len} numbers betwren ${min} and ${max}`);
+    }
+    const res: number[] = [];
+    while(res.length<len) {
+        const rn = RNG(min, max);
+        // CONTROLLO SE IL NUMERO RANDOMICO GENERATO è GIà PRESENTE
+        // ALL'INTERNO DELL'ARRAY SE è VERO RICOMINCIA IL CICLO WHILE
+        // SE è FALSO ESEGUE IL PUSH
+        if(res.includes(rn)){
+            continue;
+        }
+        res.push(rn);
+    }
+    return res;
+}
+
+const ruote = ["Bari", "Cagliari", "Firenze", "Genova", "Milano", "Napoli", "Palermo", "Roma", "Torino", "Venezia", "Nazionale"];
+
+const estrazioni: {[ruota: string]: number[]} = {};
+
+for(const ruota of ruote){
+    const estrazione = RNGSequence(5, 1, 100);
+    estrazioni[ruota] = estrazione;
+}
+console.log(JSON.stringify(estrazioni, null, 2))
+// console.log(myArr);
